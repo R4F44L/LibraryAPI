8ae30a47b992e850518a0e03a338ddc44beb2bbb
Rafal <rc@ampliapps.com>
2020-05-08 08:34:15 +0200

    structure add

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(
+        "Author", related_name="books"
+    )
+    ISBN = models.IntegerField()
+    category = models.CharField(max_length=50)
+    release_date = models.DateField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    birth_date = models.DateField()
